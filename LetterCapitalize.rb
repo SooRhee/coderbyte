5cba34122bc406f8ed0a49e23a477230233ede8b
SooRhee <asoorhee@gmail.com>
2015-07-29 14:37:12 +0900

    Create LetterCapitalize.rb

---
+++ LetterCapitalize.rb
@@ -0,0 +1,12 @@
+def LetterCapitalize(str)
+  
+
+  # code goes here
+  return str.split.each{|x| x.capitalize!}.join(' ')
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterCapitalize(STDIN.gets)           
+
